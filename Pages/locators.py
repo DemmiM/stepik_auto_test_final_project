ecf42fbb4bca4e185131047fb8c3abbe65c40a23
DemmiM <maxim00_000@mail.ru>
2021-09-10 22:17:07 +0600

    Add product to basket from product page

--- Pages/locators.py
+++ Pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#login_form")
     LOG_IN_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div h1")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, "div.alert div p strong")
